fc3fc0f87a2b5a1d5f5d9438dc36732a9978a547
yageshwari67 <yageshwari67@gmail.com>
2026-08-05 22:38:49 -0700

    Add files via upload

---
+++ Variables .java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaring variables
+        int age = 20;
+        double height = 5.8;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "John";
+
+        // Displaying variable values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
