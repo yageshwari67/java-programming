0e696a54891f42bf34de8a75e3037fb90e5b9b25
yageshwari67 <yageshwari67@gmail.com>
2026-08-05 23:11:13 -0700

    Add files via upload

---
+++ SequenceStatement .java
@@ -0,0 +1,13 @@
+public class SequenceStatement {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int product = a * b;
+
+        System.out.println("Value of a = " + a);
+        System.out.println("Value of b = " + b);
+        System.out.println("Sum = " + sum);
+        System.out.println("Product = " + product);
+    }
+}
