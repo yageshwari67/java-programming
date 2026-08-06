48c412a8386e2b93a116fbf4d07ae56c0e24822f
yageshwari67 <yageshwari67@gmail.com>
2026-08-05 22:31:00 -0700

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,14 @@
+public class Relational {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 10;
+        b = 20;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
+
