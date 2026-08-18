681e217d1db4d4e70fc8b6bf7d8fd5e1007a0099
yageshwari67 <yageshwari67@gmail.com>
2026-08-18 14:51:19 +0530

    Add files via upload

---
+++ TernaryExample.java
@@ -0,0 +1,12 @@
+public class TernaryExample {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        // Ternary operator
+        int largest = (a > b) ? a : b;
+
+        System.out.println("Largest number = " + largest);
+    }
+}
